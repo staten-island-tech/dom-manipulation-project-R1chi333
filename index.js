f8c62b69cf05b2e30f603038585c0de2747d2ee6
R1chi333 <angelaguo006@gmail.com>
2022-11-02 14:00:36 -0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const DOMSelectors = {};
+const DOMSelectors = {
+  sectionA: document.getElementById("sectionA"),
+  input: document.querySelector("#input"),
+  button: document.getElementById("btn"),
+};
+
+DOMSelectors.button.addEventListener("click", function () {
+  DOMSelectors.sectionA.insertAdjacentHTML("beforebegin", `<p>${input}</p>`);
+});
